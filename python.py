e1550fdaed383871dfa136bab0f9bc2079be7249
angang-li <angang-li@hotmail.com>
2018-04-27 11:37:10 -0500

    Add notes on Python data types and flow control

---
+++ python.py
@@ -0,0 +1,56 @@
+# Python notes
+
+####################
+# Data types
+    # single value
+        integer
+        float
+        boolean
+        string
+    # collections
+        list
+        tuple
+        set
+        dictionary
+
+# Operations on integer and float
+    // # truncating division
+    %  # remainder
+    ** # exponentiation
+
+# Operators for boolean
+    == # note assignment vs equality test
+    !=
+    and
+    or
+    not
+
+# Built-in methods on string
+    .capitalize() # capitalizes the first character
+    .lower() # makes the entire string lowercase
+    .upper() # makes the entire string uppercase
+    .title() # capitalizes every word in a string
+
+    .strips(' ') # strip away characters from the right side 
+    .lstrip(' ') # strip away characters starting from the left
+
+    .isalpha # check if all of the characters are alphabetical
+    .isnumeric # check if the string is a number
+
+####################
+# Flow control
+# What if
+    if 'a' in 'abcd':
+        ...
+    elif ...:
+        ...
+    else:
+        ...
+
+# While loop
+    while ...:
+        ...
+
+# For loop:
+    for i in range(1, 5):
+        ...
